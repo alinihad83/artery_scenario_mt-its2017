76926671ea74ce269ba2db6716093a4865e09f81
Raphael Riebl <raphael.riebl@thi.de>
2017-03-20 13:37:22 +0100

    add MT-ITS 2017 scenario
    Change-Id: I2269f8e1622e3ec2de4531de656c706843d73583

---
+++ mt-its2017.py
@@ -0,0 +1,26 @@
+import storyboard
+import timeline
+
+
+def createStories():
+        # Create TtcCondition
+        ttcCondition = storyboard.TtcCondition(1.5, 200)  # ttc 1.5sec | radius 200m
+        speedConditonGreater = storyboard.SpeedDifferenceConditionFaster(5.55556)  # 20 km/h
+        ttcSignalEffect = storyboard.SignalEffect("irc")
+        speedAndTTC = storyboard.AndCondition(ttcCondition, speedConditonGreater)
+
+        story = storyboard.Story(speedAndTTC, [ttcSignalEffect])
+
+        # Vehicle stops at second 20
+        timeToStop = storyboard.TimeCondition(timeline.seconds(20))
+        vehicleToStop = storyboard.CarSetCondition("flow1.0")
+        vehicleAndTimeToStop = storyboard.AndCondition(timeToStop, vehicleToStop)
+        stopEffect = storyboard.StopEffect()
+
+        stopStory = storyboard.Story(vehicleAndTimeToStop, [stopEffect])
+
+        # Register Stories at the Storyboard
+        board.registerStory(story)
+        board.registerStory(stopStory)
+
+        print("Stories loaded!")
